8f15e4e0abe9a857a192b80f73fed78739fb17b7
Julian Liebl <jujulian1987@gmail.com>
2016-09-05 16:16:39 +0200

    added propper shutdown handling

--- register.py
+++ register.py
@@ -6,6 +6,10 @@
 import logging
 import socket
 import sys
+import signal
+import atexit
+import os
+
 from time import sleep
 
 from zeroconf import ServiceInfo, Zeroconf
@@ -22,11 +26,39 @@ def get_ip():
         s.close()
     return IP
 
+def write_pid():
+    pid = os.getpid()
+    op = open("./wvg-zeroconf.pid","w")
+    op.write("%s" % pid)
+    op.close()
+
+def signal_handler(signal, frame):
+    sys.exit(0)
+
+
+def on_known():
+    pass
+
+
+def on_unknown():
+    pass
+
+
+def on_shutdown():
+    print("Unregistering...")
+    zeroconf.unregister_service(info)
+    zeroconf.close()
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     if len(sys.argv) > 1:
         assert sys.argv[1:] == ['--debug']
         logging.getLogger('zeroconf').setLevel(logging.DEBUG)
+
+    write_pid()
+    
+    signal.signal(signal.SIGTERM, signal_handler)
+    atexit.register(on_shutdown)
 
     desc = {'path': '/~paulsm/'}
 
@@ -36,14 +68,10 @@ if __name__ == '__main__':
                        desc, socket.gethostname())
 
     zeroconf = Zeroconf()
-    print("Registration of a service, press Ctrl-C to exit...")
+    print("Registration of a service")
     zeroconf.register_service(info)
     try:
         while True:
             sleep(0.1)
     except KeyboardInterrupt:
         pass
-    finally:
-        print("Unregistering...")
-        zeroconf.unregister_service(info)
-        zeroconf.close()
